5a80325e662cb9d2f6a2561d1a96e9f39717b43c
diuca puig <diucapuig@gmail.com>
2023-03-05 20:08:17 +0000

    created

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog ", "My granma ", "His turtle ", "My bird "];
+  let what = ["eat ", "pissed ", "crushed ", "broked "];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let string1 = who[Math.floor(Math.random() * who.length)];
+  let string2 = what[Math.floor(Math.random() * what.length)];
+  let string3 = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = string1 + string2 + string3;
+  document.getElementById("excuse").innerHTML = excuse;
 };
